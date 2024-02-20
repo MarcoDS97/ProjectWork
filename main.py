c659e5f4490fc2cf8846600d94867404076de1af
criste95 <cri.ste95@gmail.com>
2024-02-20 17:29:50 +0100

    update

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ height = float(input("Inserisci l'altezza in cm: "))
 weight = float(input("Inserisci il peso in kg: "))
 age = int(input("Inserisci l'età: "))
 gender = input("Inserisci il genere (M o F): ")
-activity_level = input("Inserisci il livello di attività (sedentary, lightly_active, moderately_active, hard_worker): ")
+activity_level = input("Inserisci il livello di attività (sedentario, leggermente_attivo, moderatamente_attivo, molto_attivo): ")
+goal = input("Inserisci l'obiettivo (dimagrire, mantenersi_in_forma, aumentare_massa_muscolare): ")
 
-tdee = calculate_tdee(height, weight, age, gender, activity_level)
+tdee = calculate_tdee(height, weight, age, gender, activity_level, goal)
 
 print(f"Il tuo TDEE è: {tdee} kcal")
