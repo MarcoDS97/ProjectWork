d7daa740612391d2975cdc8310abc7f4dfde414c
benaversano98 <ben.aversano@gmail.com>
2024-02-20 12:52:36 +0100

    update

--- database/create_db.py
+++ database/create_db.py
@@ -1,33 +1,40 @@
 import pandas as pd
 from pymongo import MongoClient
 
-percorso = r"csv\prodotti_in_pulizia.csv"
+percorso = r"csv\prodotti.csv"
 df1 = pd.read_csv(percorso, delimiter=';')
 percorso = r"csv\utenti.csv"
 df2 = pd.read_csv(percorso, delimiter=',')
 
 client = MongoClient("mongodb+srv://projectwork:SpeSana@cluster0.ajv3ccw.mongodb.net/")
 db = client['SpeSana']
-# collection = db['Products']
-# data1 = df1.to_dict(orient='records')
-# for e in data1:
-#     categories = e["categories"]
-#     if isinstance(categories, str):
-#         list_categories = categories.split(",")
-#         e["categories"] = [e.strip() for e in list_categories]
-# collection.insert_many(data1)
-# print(collection)
-# collection = db['Users']
-# data2 = df2.to_dict(orient='records')
-# collection.insert_many(data2)
+collection = db['Products']
+data1 = df1.to_dict(orient='records')
+for e in data1:
+    categories = e["categories"]
+    if isinstance(categories, str):
+        list_categories = categories.split(",")
+        e["categories"] = [e.strip() for e in list_categories]
+    list_numbers = {"energy-kcal_100g", "fat_100g","saturated-fat_100g", "carbohydrates_100g", "sugars_100g", "fiber_100g", "proteins_100g", "salt_100g", "sodium_100g"}
+    for string in list_numbers:
+        numero_stringa = e[string]
+        if isinstance(numero_stringa, str):
+            numero_float = float(numero_stringa.replace(",", "."))
+            e[string] = numero_float
+if "Products" in db.list_collection_names():
+    collection.drop()
+collection.insert_many(data1)
 
-prodotti = db['Products']
-category = "Snacks"
-# Query per cercare i prodotti di una certa categoria
-products_in_category = list(prodotti.find({"categories": "Snacks"}))
+
+collection = db['Users']
+data2 = df2.to_dict(orient='records')
+for e in data2:
+    favorites = e["Favorites"]
+    if isinstance(favorites, str):
+        list_favorites = favorites.split(",")
+        e["Favorites"] = [e.strip() for e in list_favorites]
+if "Users" in db.list_collection_names():
+    collection.drop()
+collection.insert_many(data2)
 
 
-# Stampa dei prodotti trovati
-print("Prodotti nella categoria", category)
-for product in products_in_category[:10]:
-    print(product["product_name"])
