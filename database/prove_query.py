d08bd632afd4e9e8ebd93f71619f5618f7db9df5
benaversano98 <ben.aversano@gmail.com>
2024-02-26 13:38:16 +0100

    update

--- database/prove_query.py
+++ database/prove_query.py
@@ -1,9 +1,28 @@
+# from pymongo import MongoClient
+# import pandas as pd
+#
+# client = MongoClient("mongodb+srv://projectwork:daita12@cluster0.hqm86xs.mongodb.net/")
+# db = client['SpeSana']
+# prodotti = db['Products']
+# parola_cercata = "spaghetti"
+# risultato = list(prodotti.find({"categories": {"$regex": f".*{parola_cercata}.*", "$options": "i"}}).sort("unique_scans_n", -1))
+#
+# for e in risultato:
+#     print(e)
+
 from pymongo import MongoClient
+import pandas as pd
 
 client = MongoClient("mongodb+srv://projectwork:daita12@cluster0.hqm86xs.mongodb.net/")
 db = client['SpeSana']
-prodotti = db['Products']
-risultato = prodotti.find({"nutriscore_grade": "a"}).sort("nutriscore_score").limit(10)
+users = db['Users']
+
+risultato = utenti.find({})
+
+utenti = pd.DataFrame(risultato)
+
+
+
 
 # risultato = prodotti.find({"categories": "Prodotti spalmabili"}).sort("unique_scans_n", -1).limit(10)
 # for e in risultato:
